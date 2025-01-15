37eaa987fcf41550581b03cee71da7b8ab81d084
manu2345-Cpu <manu111dwivedi@gmail.com>
2025-01-15 14:14:46 +0530

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -88,7 +88,7 @@ async def progress_bar(current, total, reply, start):
             bar_length = 11
             completed_length = int(current * bar_length / total)
             remaining_length = bar_length - completed_length
-            progress_bar = "▓" * completed_length + "▒" * remaining_length
+            progress_bar = "🔴" * completed_length + "🟧" * remaining_length
             
             try:
                 await reply.edit(f'`\n ╭──⌯════𝐁𝐨𝐭 𝐒𝐭𝐚𝐭𝐢𝐜𝐬═════⌯──╮ \n├⚡ {progress_bar} |﹝{perc}﹞ \n├🚀 Speed ➤ {sp} \n├📟 Processed ➤ {cur}\n├🧲 Size - ETA ➤ {tot} - {eta} \n╰─═══ ✨🦋PELU HARSHU🦋✨ ═══─╯`\n') 
